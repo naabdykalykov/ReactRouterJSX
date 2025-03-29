8f02685608b50718c8f85eb1de8dcbbc505c42b5
nursultan <znslt24@gmail.com>
2025-03-29 12:31:03 +0300

    how to use Link from react-router?

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      Home Page
+      {/* <a href="http://localhost:5173/about">about</a> */}
+      <Link to={"/"}>Home</Link>
+      <Link to={"about"}>About</Link>
+      <Link to={"cart"}>Cart</Link>
+    </div>
+  );
 }
 
 export default Home;
